c51aba3a95ab9390af9fabc92de1a1cb22643484
matheoafonso <fellton433@gmail.com>
2023-11-15 09:29:33 +0100

    exo2

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
-alert("HelloWord");
+//chaine de caractère
+var MavariableString = "toto";
+//nombre 
+var MavariableNumerique = 100;
+//des tableaux
+var Mavariabletableau = ['Apple', 'Banana'];
+//des objets (ici la fenetre de navigateur )
+var MavariableObjet = window
+
+alert(MavariableString + MavariableNumerique + Mavariabletableau[0]+ MavariableObjet.location.pathname);
